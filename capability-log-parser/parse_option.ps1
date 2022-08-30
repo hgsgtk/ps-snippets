8177944efc9e1b94b196079fc9ac7707c8715902
hgsgtk <kazuki@autify.com>
2022-08-30 15:55:29 +0900

    refactor: don't use alias

--- capability-log-parser/parse_option.ps1
+++ capability-log-parser/parse_option.ps1
@@ -3,8 +3,8 @@
 $basicAuths = Select-String --custom-basic-auth-configs ./selenium.log `
     # $_ = data got by Get-Process https://nabelog.org/400/
     # ForEach-Object https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/foreach-object?view=powershell-7.2
-    | % {$_ -replace "--custom-basic-auth-configs=", ""} `
-    | % {$_.ToString().Split(" ").Where({$_ -ne ""})[1].Split(',') -replace '"', ''}
+    | ForEach-Object {$_ -replace "--custom-basic-auth-configs=", ""} `
+    | ForEach-Object {$_.ToString().Split(" ").Where({$_ -ne ""})[1].Split(',') -replace '"', ''}
     | Where-Object ({ "" -ne $_})
 Write-Output $basicAuths
 
