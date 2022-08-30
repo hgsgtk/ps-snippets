98982c9af474dd53c2fd3b51d968fe5d1af4523d
hgsgtk <kazuki@autify.com>
2022-08-30 15:36:43 +0900

    ci: include PSAvoidUsingCmdletAliases

--- capability-log-parser/parse_option.ps1
+++ capability-log-parser/parse_option.ps1
@@ -4,7 +4,7 @@ $basicAuths = Select-String --custom-basic-auth-configs ./selenium.log `
     # $_ = data got by Get-Process https://nabelog.org/400/
     # ForEach-Object https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/foreach-object?view=powershell-7.2
     | ForEach-Object {$_ -replace "--custom-basic-auth-configs=", ""} `
-    | ForEach-Object {$_.ToString().Split(" ").Where({$_ -ne ""})[1].Split(',') -replace '"', ''}
+    | % {$_.ToString().Split(" ").Where({$_ -ne ""})[1].Split(',') -replace '"', ''}
     | Where-Object ({ "" -ne $_})
 Write-Output $basicAuths
 
